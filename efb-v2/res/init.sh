a0235344bb674b191f52595c032b06ee69b7134c
bmw@kali <wowjoint@gmail.com>
2021-04-13 00:01:43 +0800

    史诗级的改进，主要是读取UID后，再利用 su-exec 来运行，权限得以改善

--- efb-v2/res/init.sh
+++ efb-v2/res/init.sh
@@ -1,19 +1,32 @@
 #!/usr/bin/with-contenv bash
-# export EFB_DATA_PATH='/opt/app/efb_config'
+# export EFB_DATA_PATH='/efb/config'
 
+bmw(){
 mkdir -p ${EFB_DATA_PATH}
-cp -r -u /opt/app/bak_config/. ${EFB_DATA_PATH}
-chown -R 1000:1000 ${EFB_DATA_PATH}
-sed -i "s/token123456/$TOKEN/g" ${EFB_DATA_PATH}/profiles/${PROFILE}/blueset.telegram/config.yaml 
-sed -i "s/admin123456/$ADMIN/g" ${EFB_DATA_PATH}/profiles/${PROFILE}/blueset.telegram/config.yaml
+cp -r -u /$BMWHOME/bak_config/. ${EFB_DATA_PATH}
 
-#ln -s ${EFB_DATA_PATH} /efb
+if test -n "$UID";then 
+    chown -R $UID /$BMWHOME
+fi
 
-if test -z "$PROXYURL";then 
-    ehforwarderbot -p ${PROFILE}
-else
+if test -n "$TOKEN";then 
+    sed -i "s/token123456/$TOKEN/g" ${EFB_DATA_PATH}/profiles/${PROFILE}/blueset.telegram/config.yaml
+fi
+
+if test -n "$ADMIN";then 
+    sed -i "s/admin123456/$ADMIN/g" ${EFB_DATA_PATH}/profiles/${PROFILE}/blueset.telegram/config.yaml
+fi
+
+if test -n "$PROXYURL";then 
     sed -i "s/^#//g" ${EFB_DATA_PATH}/profiles/${PROFILE}/blueset.telegram/config.yaml
     sed -i "s/127.0.0.1/$PROXYURL/g" ${EFB_DATA_PATH}/profiles/${PROFILE}/blueset.telegram/config.yaml
-    sed -i "s/1024/$PROXYPORT/g" ${EFB_DATA_PATH}/profiles/${PROFILE}/blueset.telegram/config.yaml
-    ehforwarderbot -p ${PROFILE}
 fi
+
+if test -n "$PROXYPORT";then 
+    sed -i "s/^#//g" ${EFB_DATA_PATH}/profiles/${PROFILE}/blueset.telegram/config.yaml
+    sed -i "s/1024/$PROXYPORT/g" ${EFB_DATA_PATH}/profiles/${PROFILE}/blueset.telegram/config.yaml
+fi
+/sbin/su-exec $UID /usr/local/bin/python /usr/local/bin/ehforwarderbot -p ${PROFILE}
+}
+
+bmw
